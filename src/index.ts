137fbbc3e79daa7e7ba6ee45278c625e4259cb60
Luke Carr <luke_jcarr@icloud.com>
2021-11-17 14:51:25 +0000

    📝 Added missing jsdocs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,47 @@
 import type { ServerResponse } from 'http'
 
+/**
+ * An RFC 7807 compliant error.
+ */
 export type Error = {
+  /**
+   * A URI reference that identifies the problem type.
+   */
   type: string
+  /**
+   * A short, human-readable summary of the problem type.
+   *
+   * This should not change between occurrences of the same problem type,
+   * except for purposes of localization.
+   */
   title: string
+  /**
+   * The HTTP status code for this ocurrence of the problem type.
+   */
   status: number
+  /**
+   * A human-readable explanation specific to this ocurrence of the
+   * problem type.
+   */
   detail: string
+  /**
+   * A URI reference that identifies the specific occurrence of the problem
+   * type.
+   */
   instance: string
 };
 
+/**
+ * The MIME/media type for RFC 7807 error responses (in JSON).
+ */
 export const mime = 'application/problem+json'
 
+/**
+ * Generates an RFC 7807 compliant error and sends it as an HTTP response.
+ *
+ * @param res The `http.ServerResponse` object to send the error to.
+ * @param err The error details to send.
+ */
 export function withError (res: ServerResponse, err: Partial<Error>): void {
   if (typeof err.status !== 'undefined') {
     res.statusCode = err.status
